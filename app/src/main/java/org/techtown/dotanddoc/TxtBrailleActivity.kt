9c4f81abe4c55a322ee9fb87a5c0a08a6786271e
Youjung Heo <hujumee27@gmail.com>
2021-10-16 07:45:13 +0900

    add saving extra file in external storage

--- app/src/main/java/org/techtown/dotanddoc/TxtBrailleActivity.kt
+++ app/src/main/java/org/techtown/dotanddoc/TxtBrailleActivity.kt
@@ -1,32 +1,64 @@
 package org.techtown.dotanddoc
 
+import android.annotation.SuppressLint
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.Intent.*
+import android.net.Uri
 import android.os.*
+import android.os.Environment.*
+import android.provider.DocumentsContract
 import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import androidx.documentfile.provider.DocumentFile
 import com.amplifyframework.core.Amplify
 import com.amplifyframework.storage.options.StorageDownloadFileOptions
 import com.amplifyframework.storage.options.StorageUploadFileOptions
-import java.io.File
+import java.io.*
+import java.nio.file.Files.createDirectory
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.concurrent.timer
+
 
 class TxtBrailleActivity : AppCompatActivity() {
 
     private var tryDownload: Timer? = null
     private val progressDialog = CustomProgressDialog()
 
-    lateinit var failureAlert: AlertDialog.Builder
+    val FailureIntent = Intent(this, MainActivity::class.java)
 
+    lateinit var failureAlert: AlertDialog.Builder
     private val handler = object : Handler(Looper.getMainLooper()) {
+        @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
+        fun showAlert() {
+            FailureIntent.addFlags(FLAG_ACTIVITY_CLEAR_TOP)
+            //실패해서 메인화면으로 넘어가는 경우 메인화면 외 모든 스택 제거
+
+            failureAlert.setTitle("다운로드에 실패했습니다")
+            failureAlert.setMessage("다시 시도하시겠습니까?")
+            failureAlert.setPositiveButton("에") { dialogInterface: DialogInterface?, i: Int ->
+                Log.d("MyAmplifyApp", "다운로드 재시도")
+                downloadFile(downloadKey)
+                finish()
+            }
+            failureAlert.setNegativeButton("아니오") { dialogInterface: DialogInterface?, i: Int ->
+                startActivity(FailureIntent)
+                finish()
+            }
+            failureAlert.show()
+        }
     }
 
     lateinit var downloadKey: String
+
+    // Request code for creating a brf file.
+    val CREATE_FILE = 1
 
     @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -90,15 +122,12 @@ class TxtBrailleActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.KITKAT_WATCH)
     private fun downloadFile(uploadedKey: String) {
         // 저장위치: 내부 저장소 => 수정해야함
-        val file = File("${applicationContext.filesDir}/${downloadKey.substring(9)}")
+        val fileDir = "${applicationContext.filesDir}/${downloadKey.substring(9)}"
+        val file = File(fileDir)
 
-        Log.d("MyAmplifyApp", "${applicationContext.filesDir}/${downloadKey.substring(9)}")
+        Log.d("MyAmplifyApp", "save: $fileDir")
 
         val CompleteIntent = Intent(this, SaveCompleteActivity::class.java)
-
-        val FailureIntent = Intent(this, MainActivity::class.java)
-        FailureIntent.addFlags(FLAG_ACTIVITY_CLEAR_TASK)
-        //실패해서 메인화면으로 넘어가는 경우 메인화면 외 모든 스택 제거
 
         val options = StorageDownloadFileOptions.defaultInstance()
 
@@ -115,6 +144,11 @@ class TxtBrailleActivity : AppCompatActivity() {
                     //로딩 dialog 삭제
                     progressDialog.dialog.dismiss()
 
+                    //외부 저장소 저장
+                    val existFile = openFileInput(downloadKey.substring(9))
+                    copyFile(existFile,
+                        "${getExternalFilesDir(DIRECTORY_DOWNLOADS)}/new${downloadKey.substring(9)}")
+
                     //뷰 전환
                     startActivity(CompleteIntent)
                     cancel()
@@ -130,21 +164,16 @@ class TxtBrailleActivity : AppCompatActivity() {
                 cancel()
 
                 //실패한 경우 alert show
-                handler.post() {
-                    failureAlert.setTitle("다운로드에 실패했습니다")
-                    failureAlert.setMessage("다시 시도하시겠습니까?")
-                    failureAlert.setPositiveButton("에") { dialogInterface: DialogInterface?, i: Int ->
-                        Log.d("MyAmplifyApp", "다운로드 재시도")
-                        downloadFile(downloadKey)
-                        finish()
-                    }
-                    failureAlert.setNegativeButton("아니오") { dialogInterface: DialogInterface?, i: Int ->
-                        startActivity(FailureIntent)
-                        finish()
-                    }
-                    failureAlert.show()
-                }
+                handler.showAlert()
             }
+        }
+    }
+
+    fun copyFile(inputStream: InputStream, filePath: String) {
+        val saveFile = File(filePath)
+
+        saveFile.outputStream().use { fileOutput ->
+            inputStream.copyTo(fileOutput)
         }
     }
 }
